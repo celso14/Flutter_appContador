9668e643553432d50fdcbc017982be17bb43000e
celso14 <celso.botelho14@gmail.com>
2022-05-11 00:08:43 -0300

    Finalização do primeiro app flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,70 +10,148 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int count = 0;
+  int total = 0;
+
+  Color colorTextAlert() {
+    if (isEmpty || isFull) {
+      return Colors.red;
+    } else {
+      return Colors.white;
+    }
+  }
+
   void decrement() {
-    print("decrement");
+    setState(() {
+      count--;
+    });
   }
 
   void increment() {
-    print("Increment");
+    setState(() {
+      count++;
+      total++;
+    });
   }
+
+  bool get isEmpty => count == 0;
+  bool get isFull => count == 20;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.red,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text(
-            "Pode entrar",
-            style: TextStyle(
-              fontSize: 32,
-              color: Colors.white,
-              fontWeight: FontWeight.w700,
-            ),
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/img/imagesena.jpg'),
+            fit: BoxFit.cover,
           ),
-          const Text(
-            "0",
-            style: TextStyle(
-              fontSize: 100,
-              color: Colors.white,
-              fontWeight: FontWeight.w700,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "Número total de pessoas",
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.white,
+                fontWeight: FontWeight.w700,
+              ),
             ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextButton(
-                onPressed: decrement, // não se passa os parenteses
-                child: Text(
-                  "Saiu",
-                  style: TextStyle(
-                    color: Colors.black,
-                  ),
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Text(
+                "$total",
+                style: const TextStyle(
+                  fontSize: 25,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold
                 ),
               ),
-              TextButton(
-                onPressed:
-                    increment, // não se passa os parenteses, se não está chamando o retorno da função
-                child: Text(
-                  "Entrou",
-                  style: TextStyle(
-                    color: Colors.black,
-                  ),
+            ),
+            Text(
+              isFull ? "Lotado" : "Pode entrar!",
+              style: TextStyle(
+                fontSize: 32,
+                color: colorTextAlert(),
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(60),
+              child: Text(
+                "$count",
+                style: TextStyle(
+                  fontSize: 100,
+                  color: colorTextAlert(),
+                  fontWeight: FontWeight.w700,
                 ),
               ),
-            ],
-          ),
-        ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: isEmpty ? Colors.grey : Colors.white,
+                    fixedSize: const Size(100, 100),
+                    primary: Colors.red,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(24),
+                    ),
+                  ),
+                  onPressed:
+                      isEmpty ? null : decrement, // não se passa os parenteses
+                  child: const Text(
+                    "Saiu",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  width: 40,
+                ),
+                TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: isFull ? Colors.grey : Colors.white,
+                    fixedSize: const Size(100, 100),
+                    primary: Colors.red,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(24),
+                    ),
+                  ),
+                  onPressed: isFull
+                      ? null
+                      : increment, // não se passa os parenteses, se não está chamando o retorno da função
+                  child: const Text(
+                    "Entrou",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
